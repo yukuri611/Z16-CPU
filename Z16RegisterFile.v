88fefc2f30472fc41f41a45623cdb44a1a32515c
yukuri611 <yukiiw611@gmail.com>
2025-12-25 11:02:40 +0900

    create Register File module

--- Z16RegisterFile.v
+++ Z16RegisterFile.v
@@ -9,8 +9,18 @@ module Z16RegisterFile(
 
     input wire[3:0] i_rd_addr,
     input wire i_rd_we,
-    input wire[15:0] o_rd_data,
+    input wire[15:0] i_rd_data
 );
 
+    reg[15:0] mem[15:0];
+
+    assign o_rs1_data = (i_rs1_addr == 4'h0) ? 16'h0000: mem[i_rs1_addr];
+    assign o_rs2_data = (i_rs2_addr == 4'h0) ? 16'h0000: mem[i_rs2_addr];
+
+    always @(posedge i_clk) begin
+        if (i_rd_we) begin
+            mem[i_rd_addr] <= i_rd_data;
+        end
+    end
 
 endmodule
