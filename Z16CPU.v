171570b3f0e296dd97895ffe994043583506fe9c
yukuri611 <yukiiw611@gmail.com>
2025-12-27 09:42:40 +0900

    implement fetch mechanism

--- Z16CPU.v
+++ Z16CPU.v
@@ -3,9 +3,22 @@ module Z16CPU(
     input wire i_rst
 );
     
+    reg [15:0] r_pc;
+    wire [15:0] w_instr;
+
+    always @(posedge i_clk) begin
+        if (i_rst) begin
+            r_pc <= 16'h0000;
+        end else begin
+            r_pc <= r_pc + 16'h0002;
+        end
+    end
+
+    
+
     Z16InstrMem InstrMem(
-        .i_addr(),
-        .o_instr()
+        .i_addr(r_pc),
+        .o_instr(w_instr)
     );
     
     Z16DataMem DataMem(
